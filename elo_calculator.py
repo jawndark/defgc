85b4672f056c9cba02ad9969458b985c7e8622d9
ehitchings <ehitchings77@gmail.com>
2025-05-15 14:49:58 -0400

    Character ELO calculations weighted by the change in Player ELO now.

--- elo_calculator.py
+++ elo_calculator.py
@@ -21,11 +21,11 @@ BASE_RATINGS = {'Boog': 1500,
 
 MOCK_TEAMS = {'Boog': ['Hatoyan', 'Tsuki', 'Gyanta'],
  'Tension': ['Cocorn', 'Hatoyan','Zacky Wild'],
- 'Poke': ['Kyanta', 'Rare', 'Gyanta'],
- 'Squid': ['Garasha', 'Kyanta', 'Sendou'],
+ 'Poke': ['Kyanta', 'Jakor', 'Gyanta'],
+ 'Squid': ['Garasha', 'Chihiro', 'Sendou'],
  'Cosmic': ['M. Michelle', 'Anna', 'Investigator'],
- 'Valentine': ['Natanee', 'Myusha', 'Anna'],
- 'Jawn': ['Nanatsu', 'Rare', 'Robo Azuma'],
+ 'Valentine': ['Natanee', 'Myusha', 'Hatoyan'],
+ 'Jawn': ['Nanatsu', 'Rare', 'Natanee'],
  'D-Cint': ['Hatoyan', 'Michelle', 'Gyanta'],
  'Parappa': ['Hatoyan', 'Anna', 'Chihiro']}
 
@@ -38,6 +38,7 @@ def get_rating_sheet(path=r"./Data/elo_ratings.csv"):
     try:
         df = pd.read_csv(path, index_col=0, header=0)
         df.index.name = 'Player'
+        # df = df.sort_index(ascending=False)
     except FileNotFoundError:
         print(f"File not found: {path}")
         return None
@@ -128,28 +129,45 @@ if __name__ == "__main__":
     for _, x in df.iterrows():
         p1 = x['Player1']
         p2 = x['Player2']
-        p1_rating = player_rating_dict.get(p1, 1500)
-        p2_rating = player_rating_dict.get(p2, 1500)
-        p1_rating, p2_rating = elo_calculator.update_ratings(p1_rating, p2_rating, x['P1Result'])   
+        old_p1_rating = player_rating_dict.get(p1, 1500)
+        old_p2_rating = player_rating_dict.get(p2, 1500)
+        # print(f'P1: {p1}, P2: {p2}')
+        # print(f"P1 Rating: {old_p1_rating}, P2 Rating: {old_p2_rating}")
+        p1_rating, p2_rating = elo_calculator.update_ratings(old_p1_rating, old_p2_rating, x['P1Result'])   
         player_rating_dict[p1] = p1_rating
-        player_rating_dict[p2] = p2_rating          
+        player_rating_dict[p2] = p2_rating  
+        p1_rating_change = p1_rating - old_p1_rating
+        p2_rating_change = p2_rating - old_p2_rating    
         for p1_num in range(1,4):
             p1ch = x[f'P1C{p1_num}']
             for p2_num in range(1,4):
                 p2ch = x[f'P2C{p2_num}']
                 if p1ch == p2ch:
                     continue
+                # print(f" P1character: {p1ch}, P2character: {p2ch}")
+                character_1_rating = char_rating_dict.get(p1ch, 1500)
+                character_2_rating = char_rating_dict.get(p2ch, 1500)
+                # print(f"Current Character Rating: {character_1_rating}, Current Character Rating: {character_2_rating}")
+                p1ch_rating, p2ch_rating = elo_calculator.update_ratings(character_1_rating, character_2_rating, x['P1Result'])
+                char_rating_dict[p1ch] = p1ch_rating - (p1_rating_change * 1.0)
+                char_rating_dict[p2ch] = p2ch_rating - (p2_rating_change * 1.0)
+                # p1ch_rating, p2ch_rating = elo_calculator.update_ratings(current_p1ch_rating, current_p2ch_rating, x['P1Result'])
+                # print(f"Updated P1character Rating: {p1ch_rating}, Updated P2Character Rating: {p2ch_rating}")
+                # p1ch_rating += p1_rating_change
+                # p2ch_rating += p2_rating_change
+                # print(f"Adjusted P1character Rating: {p1ch_rating}, Adjusted P2Character Rating: {p2ch_rating}")
+                # current_p1ch_rating = (character_1_rating + p1_rating) / 2.0
+                # current_p2ch_rating = (character_2_rating + p2_rating) / 2.0
+                # print(f" Adjusted P1character Rating: {current_p1ch_rating}, Adjusted P2Character Rating: {current_p2ch_rating}")
 
-                p1ch_rating = (char_rating_dict.get(p1ch, 1500) + p1_rating) / 2.0
-                p2ch_rating = (char_rating_dict.get(p2ch, 1500) + p2_rating) / 2.0
-                p1ch_rating, p2ch_rating = elo_calculator.update_ratings(p1ch_rating, p2ch_rating, x['P1Result'])
-                char_rating_dict[p1ch] = p1ch_rating
-                char_rating_dict[p2ch] = p2ch_rating
+                # print(f" Updated P1character Rating: {p1ch_rating}, Updated P2Character Rating: {p2ch_rating}")
+
+                # char_rating_dict[p2ch] = character_2_rating + ((p2ch_rating - current_p2ch_rating) * 0.5)
     rdf = pd.DataFrame.from_dict(char_rating_dict, orient='index', columns=['Rating']).reset_index(names='Character')
     rdf['Rating'] = rdf['Rating'].round(2)
     rdf = rdf.sort_values(by='Rating', ascending=False).reset_index(drop=True)   
     print(rdf)
-    rdf.to_csv(r"./Data/player_character_ratings.csv", index=False)
+    rdf.to_csv(r"./Data/player_character_ratings_weighted.csv", index=False)
     prdf = pd.DataFrame.from_dict(player_rating_dict, orient='index', columns=['Rating']).reset_index(names='Player')
     prdf['Rating'] = prdf['Rating'].round(2)
     prdf = prdf.sort_values(by='Rating', ascending=False).reset_index(drop=True)   
